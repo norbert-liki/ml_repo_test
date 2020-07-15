0fad7be5279fb88666d35ff770abc64cb8421cab
Norbert Liki <no_liki@LAPTOP-JOV4M20K.localdomain>
2020-07-15 21:25:46 +0200

    test build

---
+++ ml_repo_test/commons/alma.py
@@ -0,0 +1,7 @@
+import datetime
+
+print(datetime.datetime.now())
+
+
+def add(a, b):
+    return a + b
--- scripts/build-triggers/_deploy-triggers.sh
+++ scripts/build-triggers/_deploy-triggers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ENV="dev"
-projects=("ml_repo_test")
+projects=("ml_repo_test" "kutya")
 
 for project in "${projects[@]}"
 do
-	gcloud beta builds triggers import --source=$project-$ENV-build-master.yaml
+	#gcloud beta builds triggers import --source=$project-$ENV-build-master.yaml
 	gcloud beta builds triggers import --source=$project-$ENV-build-dev.yaml
-	gcloud beta builds triggers import --source=$project-$ENV-build-pr.yaml
+	#gcloud beta builds triggers import --source=$project-$ENV-build-pr.yaml
 done
